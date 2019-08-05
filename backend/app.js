a3cf424d570b05b077705625002c06bf967545f1
sarahysh12 <saraysh22@gmail.com>
2019-08-04 19:08:55 -0700

    add get and post endpoint to backend

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,43 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('fist middleware');
-    next();
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
-    res.send('hello from express');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+  next();
 });
+
+app.use('api/posts',(req, res, next) => {
+    const posts = [
+      {
+        id:'adkajsfahte',
+      title:'First server-side post',
+      content:'This is coming from the server'
+    },
+      {
+        id:'asaioewbeclz',
+      title:'Second server-side post',
+      content:'This is coming from the server!!!'
+    }
+    ];
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: posts
+    });
+});
+
+app.post('api/posts',(req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully!'
+  });
+});
+
 
 module.exports = app;
